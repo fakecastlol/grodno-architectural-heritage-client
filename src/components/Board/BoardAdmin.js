43671e3b451e2a9e525178386cd48e3318a8bfa3
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-11-24 12:02:07 +0300

    added functionality to admin's board

--- src/components/Board/BoardAdmin.js
+++ src/components/Board/BoardAdmin.js
@@ -1,38 +1,54 @@
 import React, { useState, useEffect } from "react";
-import Table from 'react-bootstrap/Table'
+import BootstrapTable from 'react-bootstrap-table-next'
+import paginationFactory from 'react-bootstrap-table2-paginator'
+import * as ReactBootStrap from 'react-bootstrap'
 
 import UserService from "../../services/user.service";
 import AdminService from '../../services/admin.service'
+import Axios from "axios";
+import authHeader from "../../services/auth-header";
 
 const BoardAdmin = () => {
   const [content, setContent] = useState("");
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getUserData = async () => {
+    // AdminService.getUsers();
+    try{
+      const data = await Axios.get("https://localhost:5001/users", { headers: authHeader()});
+      console.log(data);
+      setUsers(data.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }; 
 
   const columns = [
-    {name: 'id'},
-    {name: 'email'},
-    {name: 'role'}
+    {dataField: 'id', text: "Id"},
+    {dataField: 'email', text: "Email"},
+    {dataField: 'role', text: "Role"}
   ];
 
-  const users = () =>{
-    AdminService.getUsers();
-  } 
 
   useEffect(() => {
-    UserService.getAdminBoard().then(
-      (response) => {
-        setContent(response.data);
-      },
-      (error) => {
-        const _content =
-          (error.response &&
-            error.response.data &&
-            error.response.data.message) ||
-          error.message ||
-          error.toString();
+    // UserService.getAdminBoard().then(
+    //   (response) => {
+    //     setContent(response.data);
+        getUserData();
+    //   },
+    //   (error) => {
+    //     const _content =
+    //       (error.response &&
+    //         error.response.data &&
+    //         error.response.data.message) ||
+    //       error.message ||
+    //       error.toString();
 
-        setContent(_content);
-      }
-    );
+    //     setContent(_content);
+    //   }
+      
+    // );
   }, []);
 
 
@@ -40,34 +56,17 @@ const BoardAdmin = () => {
   return (
     <div className="container">
       <header className="jumbotron">
-        <h3>{content}</h3>
+         <h3>{content}</h3>
 
-        <Table striped bordered hover >
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Email</th>
-            <th>Role</th>
-            {/* <th>columns={columns}</th> */}
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-          </tr>
-        </tbody>
-        </Table>
-
-      </header>
-
-    </div>
+        <BootstrapTable 
+        //  striped bordered hover
+          keyField="name"
+          data={users}
+          columns={columns}
+          pagination={paginationFactory()}
+        />
+       </header>
+      </div>
   );
 };
 
