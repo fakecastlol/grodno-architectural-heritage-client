47b649c1aef599a741bda2fe5a4ab255a7cc8d6e
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-11-30 20:08:21 +0300

    added auth constants

---
+++ src/constants/authorities.js
@@ -0,0 +1,14 @@
+const roleToString = (role) => {
+    switch (role) {
+        case 2:
+            return "admin"
+           
+        case 3:
+            return "user"
+
+        default:
+            return "user" 
+    }
+}
+
+export default roleToString
