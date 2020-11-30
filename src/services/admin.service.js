0b753af96c10a9a38272f2e355daad4ecc8792e3
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-11-30 20:09:22 +0300

    added requests to admin.service

--- src/services/admin.service.js
+++ src/services/admin.service.js
@@ -1,27 +1,47 @@
 import axios from "axios";
-import API_URL from '../constants/api.url'
-import authHeader from '../helpers/auth-header'
+import API_URL from "../constants/api.url";
+import authHeader from "../helpers/auth-header";
 
 const getUsers = () => {
-    return axios.get(API_URL + 'users');
+  return axios.get(API_URL + "users");
 };
 
-
-
 const getUser = async (id) => {
-    try {
-      return await axios.get(API_URL + "getuser", {
+  try {
+    return await axios.get(API_URL + "getuser", {
+      headers: authHeader(),
+      params: { id },
+    });
+    // console.log(data);
+  } catch (e) {}
+};
+
+const deleteUser = async (id) => {
+  try {
+    console.log(id);
+    return await axios.delete(API_URL + "delete", {
+      headers: authHeader(),
+      data: { id },
+    });
+  } catch (e) {}
+};
+
+const setRole = async (id, role) => {
+  console.log(id, role);
+  try {
+    return await axios.post(
+      API_URL + "role",
+      { id, role },
+      {
         headers: authHeader(),
-        params: { id },
-      });
-      // console.log(data);
-    } catch (e) {}
-  };
-
-
-  
+      }
+    );
+  } catch (e) {}
+};
 
 export default {
-    getUsers,
-    getUser
-}
+  getUsers,
+  getUser,
+  deleteUser,
+  setRole,
+};
