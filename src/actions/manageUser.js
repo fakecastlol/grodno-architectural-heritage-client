08189fcd5691e6d5b6fe8d0ebb080c7f2ba52b3e
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-12-02 19:30:42 +0300

    changed action

--- src/actions/manageUser.js
+++ src/actions/manageUser.js
@@ -3,8 +3,10 @@ import { GET_USER_DATA, SET_MESSAGE } from "../constants/action.types";
 import AdminService from "../services/admin.service";
 
 export const getUser = (id) => (dispatch) => {
+  console.log(id)
   return AdminService.getUser(id)
     .then((response) => {
+      console.log(response)
       dispatch({
         type: GET_USER_DATA,
         payload: response.data,
