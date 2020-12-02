4f9159a578df400c7680856926d5a6c6ed9a94fa
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-12-02 19:30:53 +0300

    added reduce

---
+++ src/reducers/profile.js
@@ -0,0 +1,20 @@
+
+import { UPDATE_USER_PROFILE } from "../constants/action.types";
+
+const initialState = { isLoading: true };
+
+export default function (state = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case UPDATE_USER_PROFILE:
+      console.log(payload)
+      return {
+        ...state,
+        user: payload,
+        isLoading: false,
+      };
+
+    default:
+      return state;
+  }
+}
