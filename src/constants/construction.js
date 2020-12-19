101d7906296af69d01e8ad2af1ffc3a02ceca4b6
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-12-19 19:45:30 +0300

    added constants for construction

---
+++ src/constants/construction.js
@@ -0,0 +1,49 @@
+export const styleToString = (style) => {
+  switch (style) {
+    case 1:
+      return "Baroque";
+
+    case 2:
+      return "Eclecticism";
+    case 3:
+      return "Moorish";
+    case 4:
+      return "Gothic Revival";
+
+    default:
+      return "";
+  }
+};
+
+export const materialToString = (material) => {
+  switch (material) {
+    case 1:
+      return "Wood";
+
+    case 2:
+      return "Brick";
+
+    default:
+      return "";
+  }
+};
+
+export const typeToString = (type) => {
+  switch (type) {
+    case 1:
+      return "Sacral";
+
+    default:
+      return "";
+  }
+};
+
+export const statusToString = (status) => {
+  switch (status) {
+    case 1:
+      return "Active";
+
+    default:
+      return "";
+  }
+};
