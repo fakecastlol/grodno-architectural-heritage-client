ca77d7d0866487f5af240866f2c6db095c37b88f
Vladimir Bobko <Uladzimir.Babko@itechart-group.com>
2021-05-30 14:30:44 +0300

    - added custom 3d layers - added constants - refactored code

--- src/constants/mapbox.js
+++ src/constants/mapbox.js
@@ -25,3 +25,53 @@ export const getConstructionType = (value) => {
       return "";
   }
 };
+
+export const layers = {
+  1: MapLayer + "streets-v10",
+  2: MapLayer + "light-v10",
+  3: MapLayer + "dark-v9",
+  4: MapLayer + "outdoors-v9",
+  5: MapLayer + "satellite-v9",
+};
+
+export const viewport = {
+  latitude: EntryPoint[0],
+  longitude: EntryPoint[1],
+  zoom: 8,
+};
+
+export const threeDimensionalLayer = {
+  id: "3d-buildings",
+  source: "composite",
+  "source-layer": "building",
+  filter: ["==", "extrude", "true"],
+  type: "fill-extrusion",
+  minzoom: 15,
+  paint: {
+    "fill-extrusion-color": "#aaa",
+
+    // use an 'interpolate' expression to add a smooth transition effect to the
+    // buildings as the user zooms in
+    "fill-extrusion-height": [
+      "interpolate",
+      ["linear"],
+      ["zoom"],
+      15,
+      0,
+      15.05,
+      ["get", "height"],
+    ],
+    "fill-extrusion-base": [
+      "interpolate",
+      ["linear"],
+      ["zoom"],
+      15,
+      0,
+      15.05,
+      ["get", "min_height"],
+    ],
+    "fill-extrusion-opacity": 0.6,
+  },
+};
+
+export const mapLayers = ['streets', 'light', 'dark', 'outdoors', 'satellite'];
--- src/containers/Map/Map.jsx
+++ src/containers/Map/Map.jsx
@@ -1,94 +1,57 @@
 import React, { useRef, useEffect, useState, useCallback } from "react";
-// import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
 import { useDispatch, useSelector } from "react-redux";
 import mapboxgl from "mapbox-gl";
-import "./Map.css";
 import Tooltip from "./Tooltip";
 import ReactDOM from "react-dom";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Button from "react-bootstrap/Button";
-import { Token, MapLayer, EntryPoint, Bounds } from "../../constants/mapbox";
+import {
+  Token,
+  EntryPoint,
+  Bounds,
+  layers,
+  viewport,
+  threeDimensionalLayer,
+  mapLayers,
+} from "../../constants/mapbox";
 import Geocoder from "@mapbox/mapbox-gl-geocoder";
-import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 import { getConstructions } from "../../actions/construction";
 import { statusToString, typeToString } from "../../constants/construction";
-import ConstructionImages from "../Construction/ConstructionImages";
 import { GetImageConstruction } from "../../constants/api.url";
-// import Stats from 'https://threejs.org/examples/jsm/libs/stats.module.js';
-import {Threebox} from 'threebox';
-import * as THREE from 'three';
+import {
+  Camera,
+  Scene,
+  DirectionalLight,
+  WebGLRenderer,
+  Matrix4,
+  Vector3,
+} from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+import "./Map.css";
+import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 
 mapboxgl.accessToken = Token;
 
 const Map = () => {
+  const mapContainerRef = useRef(null);
+  const tooltipRef = useRef(new mapboxgl.Popup({ offset: 15 }));
+  const [layer, setLayer] = useState(3);
   const dispatch = useDispatch();
-  const [markerImage, setMarkerPopup] = useState(null);
-  // constructions: constructionField,
-  // constructions,
-  // isLoading
   const objects = useSelector((state) => {
     return state.construction.constructions;
   });
+
+  const handleSwitchLayer = useCallback((value) => {
+    setLayer(value);
+  }, []);
 
   useEffect(() => {
     dispatch(getConstructions());
   }, [dispatch]);
 
-  const mapContainerRef = useRef(null);
-  const tooltipRef = useRef(new mapboxgl.Popup({ offset: 15 }));
-  //   const [viewport, setViewport] = useState("");
-  const [layer, setLayer] = useState(3);
-
-  const [viewport, setViewport] = useState({
-    latitude: EntryPoint[0],
-    longitude: EntryPoint[1],
-    zoom: 8,
-  });
-
-  const handleSwitchLayer = (value) => {
-    setLayer(value);
-  };
-
-  const handleViewportChange = useCallback(
-    (newViewport) => setViewport(newViewport),
-    []
-  );
-
-  const handleSelected = (newViewport, port) => {
-    console.log("handleSelected->newViewport", newViewport);
-    setViewport({
-      longitude: newViewport.latitude,
-      latitude: newViewport.longitude,
-      zoom: 1,
-    });
-    console.log("handleSelected->viewport", viewport);
-  };
-
-  //   const onSelected = (viewport, item) => {
-  //     this.setState({viewport});
-  //     console.log('Selected: ', item)
-  // }
-
-    // const map = new mapboxgl.Map({
-    //   container: mapContainerRef.current,
-    //   style: layers[layer],
-    //   center: [23.829529, 53.677834],
-    //   zoom: 16.5,
-    //   antialias: true,
-    //   pitch: 45,
-    //   bearing: -17.6,
-    // });
-
   // Initialize map when component mounts
   useEffect(() => {
-    const layers = {
-      1: MapLayer + "streets-v10",
-      2: MapLayer + "light-v10",
-      3: MapLayer + "dark-v9",
-      4: MapLayer + "outdoors-v9",
-      5: MapLayer + "satellite-v9",
-    };
-
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: layers[layer],
@@ -100,26 +63,6 @@ const Map = () => {
       minZoom: 11,
       maxBounds: Bounds,
     });
-    // map.z
-
-    // map.flyTo({
-    //     curve: 1,
-    //     minZoom: 1,
-    //     speed: 0.1,
-    //     screenSpeed: 0.1,
-    //     maxDuration: 10,
-    // });
-
-    // map.setCenter([23.343, 53.342]);
-
-    // var geocoder = new Geocoder({ // Initialize the geocoder
-    //   accessToken: mapboxgl.accessToken, // Set the access token
-    //   mapboxgl: mapboxgl, // Set the mapbox-gl instance
-    //   marker: false, // Do not use the default marker style
-    // });
-
-    // Add the geocoder to the map
-    // map.addControl(geocoder);
 
     map.addControl(
       new Geocoder({
@@ -147,29 +90,21 @@ const Map = () => {
       if (features.length) {
         const feature = features[0];
         const coordinates = e.lngLat.toString();
-        // e.features[0].geometry.coordinates.slice();
-        // const address = e.address.toString();
-        // const addr = map.address;
         // Create tooltip node
-        // while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-        //   coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-        // }
-        // map.setLngLat(coordinates);
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
 
-        // const tooltipNode = document.createElement("div");
-        // ReactDOM.render(
-        //   <Tooltip
-        //     feature={feature}
-        //     coordinates={coordinates}
-        //     // address={address}
-        //   />,
-        //   tooltipNode
-        // );
-        // // Set tooltip on map
-        // tooltipRef.current
-        //   .setLngLat(e.lngLat)
-        //   .setDOMContent(tooltipNode)
-        //   .addTo(map);
+        const tooltipNode = document.createElement("div");
+        ReactDOM.render(
+          <Tooltip feature={feature} coordinates={coordinates} />,
+          tooltipNode
+        );
+        // Set tooltip on map
+        tooltipRef.current
+          .setLngLat(e.lngLat)
+          .setDOMContent(tooltipNode)
+          .addTo(map);
       }
     });
 
@@ -197,7 +132,8 @@ const Map = () => {
                   ? `<div>
                       <img
                         src=${
-                          GetImageConstruction + `${construction.images[0].name}`
+                          GetImageConstruction +
+                          `${construction.images[0].name}`
                         }
                         alt=""
                         class="img-style"                     
@@ -244,143 +180,137 @@ const Map = () => {
 
       map.addLayer(
         {
-          id: "3d-buildings",
-          source: "composite",
+          id: threeDimensionalLayer.id,
+          source: threeDimensionalLayer.source,
           "source-layer": "building",
-          filter: ["==", "extrude", "true"],
-          type: "fill-extrusion",
-          minzoom: 15,
-          paint: {
-            "fill-extrusion-color": "#aaa",
-
-            // use an 'interpolate' expression to add a smooth transition effect to the
-            // buildings as the user zooms in
-            "fill-extrusion-height": [
-              "interpolate",
-              ["linear"],
-              ["zoom"],
-              15,
-              0,
-              15.05,
-              ["get", "height"],
-            ],
-            "fill-extrusion-base": [
-              "interpolate",
-              ["linear"],
-              ["zoom"],
-              15,
-              0,
-              15.05,
-              ["get", "min_height"],
-            ],
-            "fill-extrusion-opacity": 0.6,
-          },
+          filter: threeDimensionalLayer.filter,
+          type: threeDimensionalLayer.type,
+          minzoom: threeDimensionalLayer.minzoom,
+          paint: threeDimensionalLayer.paint,
         },
         labelLayerId
       );
     });
 
+    const modelAltitude = 0;
+    const modelOrigin = [EntryPoint[0], EntryPoint[1]];
+    const modelRotate = [Math.PI / 2, 0, 0];
 
+    var modelAsMercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(
+      modelOrigin,
+      modelAltitude
+    );
 
+    var modelTransform = {
+      translateX: modelAsMercatorCoordinate.x,
+      translateY: modelAsMercatorCoordinate.y,
+      translateZ: modelAsMercatorCoordinate.z,
+      rotateX: modelRotate[0],
+      rotateY: modelRotate[1],
+      rotateZ: modelRotate[2],
+      /* Since our 3D model is in real world meters, a scale transform needs to be
+       * applied since the CustomLayerInterface expects units in MercatorCoordinates.
+       */
+      scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits(),
+    };
 
+    // configuration of the custom layer for a 3D model per the CustomLayerInterface
+    var customLayer = {
+      id: "3d-model",
+      type: "custom",
+      renderingMode: "3d",
+      onAdd: function (map, gl) {
+        this.camera = new Camera();
+        this.scene = new Scene();
 
+        // create two three.js lights to illuminate the model
+        var directionalLight = new DirectionalLight(0xffffff);
+        directionalLight.position.set(0, -70, 100).normalize();
+        this.scene.add(directionalLight);
 
+        var directionalLight2 = new DirectionalLight(0xffffff);
+        directionalLight2.position.set(0, 70, 100).normalize();
+        this.scene.add(directionalLight2);
 
+        // use the three.js GLTF loader to add the 3D model to the three.js scene
+        var loader = new GLTFLoader();
+        loader.load(
+          "https://docs.mapbox.com/mapbox-gl-js/assets/34M_17/34M_17.gltf",
+          function (gltf) {
+            this.scene.add(gltf.scene);
+          }.bind(this)
+        );
+        this.map = map;
 
+        // use the Mapbox GL JS map canvas for three.js
+        this.renderer = new WebGLRenderer({
+          canvas: map.getCanvas(),
+          context: gl,
+          antialias: true,
+        });
 
+        this.renderer.autoClear = false;
+      },
+      render: function (gl, matrix) {
+        var rotationX = new Matrix4().makeRotationAxis(
+          new Vector3(1, 0, 0),
+          modelTransform.rotateX
+        );
+        var rotationY = new Matrix4().makeRotationAxis(
+          new Vector3(0, 1, 0),
+          modelTransform.rotateY
+        );
+        var rotationZ = new Matrix4().makeRotationAxis(
+          new Vector3(0, 0, 1),
+          modelTransform.rotateZ
+        );
 
+        var m = new Matrix4().fromArray(matrix);
+        var l = new Matrix4()
+          .makeTranslation(
+            modelTransform.translateX,
+            modelTransform.translateY,
+            modelTransform.translateZ
+          )
+          .scale(
+            new Vector3(
+              modelTransform.scale,
+              -modelTransform.scale,
+              modelTransform.scale
+            )
+          )
+          .multiply(rotationX)
+          .multiply(rotationY)
+          .multiply(rotationZ);
 
+        this.camera.projectionMatrix = m.multiply(l);
+        this.renderer.resetState();
+        this.renderer.render(this.scene, this.camera);
+        this.map.triggerRepaint();
+      },
+    };
 
+    map.on("style.load", function () {
+      map.addLayer(customLayer, "waterway-label");
+    });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-    
-    console.log("useEffect", viewport.latitude, viewport.longitude);
     // Clean up on unmount
     return () => map.remove();
-  }, [layer, objects, viewport.longitude, viewport.latitude]);
-
-  console.log("initial state", viewport);
+  }, [layer, objects]);
 
   return (
     <div>
       <div className="map-container" ref={mapContainerRef} />
-
-      {/* <Geocoder
-        className="geocoder"
-        onViewportChange={handleViewportChange}
-        mapboxApiAccessToken={Token}
-        onSelected={handleSelected}
-        viewport={viewport}
-        hideOnSelect={true}
-        value=""
-        // queryParams={params}
-        position="top-right"
-        inputPlaceholder= 'Search for places in Berkeley'
-      /> */}
-
       <ButtonGroup type="checkbox" aria-label="Basic example" className="menu">
-        <Button
-          variant="secondary"
-          className={`Btn-Blue-BG ${layer === 1 ? "active" : ""}`}
-          onClick={() => handleSwitchLayer(1)}
-        >
-          streets
-        </Button>
-        <Button
-          variant="secondary"
-          className={`Btn-Blue-BG ${layer === 2 ? "active" : ""}`}
-          onClick={() => handleSwitchLayer(2)}
-        >
-          light
-        </Button>
-        <Button
-          variant="secondary"
-          className={`Btn-Blue-BG ${layer === 3 ? "active" : ""}`}
-          onClick={() => handleSwitchLayer(3)}
-        >
-          dark
-        </Button>
-        <Button
-          variant="secondary"
-          className={`Btn-Blue-BG ${layer === 4 ? "active" : ""}`}
-          onClick={() => handleSwitchLayer(4)}
-        >
-          outdoors
-        </Button>
-        <Button
-          variant="secondary"
-          className={`Btn-Blue-BG ${layer === 5 ? "active" : ""}`}
-          onClick={() => handleSwitchLayer(5)}
-        >
-          satellite
-        </Button>
+        {mapLayers.map((item, index) => (
+          <Button
+            variant="secondary"
+            className={`Btn-Blue-BG ${layer === index ? "active" : ""}`}
+            onClick={() => handleSwitchLayer(index)}
+          >
+            {item}
+          </Button>
+        ))}
       </ButtonGroup>
     </div>
   );
